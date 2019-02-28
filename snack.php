765ada52ad588878931bdc7f9613aa9e9e7cb8f6
park-jihee <brs0705@naver.com>
2019-02-28 15:04:14 +0900

    view snack

--- snack.php
+++ snack.php
@@ -12,26 +12,22 @@
 				<div class="snack_bestItem">
 					<img src="image/snack_1.jpg" alt="">
 					<br>
-					<b>흥양)젤리인젤리복숭아</br>1,200원</b>
-					</img>
+					<b>흥양)젤리인젤리복숭아<br>1,200원</b>
 				</div>
 				<div class="snack_bestItem">
 					<img src="image/snack_2.jpg" alt="">
 					<br>
-					<b>빙그레)곤약젤리청포도맛</br>1,500원</b>
-					</img>
+					<b>빙그레)곤약젤리청포도맛<br>1,500원</b>
 				</div>
 				<div class="snack_bestItem">
 					<img src="image/snack_3.jpg" alt="">
 					<br>
-					<b>롯데)치즈초코파이</br>3,500원</b>
-					</img>
+					<b>롯데)치즈초코파이<br>3,500원</b>
 				</div>
 				<div class="snack_bestItem">
 					<img src="image/snack_4.jpg" alt="">
 					<br>
-					<b>쇼콜)초코드라이브딸기</br>1,200원</b>
-					</img>
+					<b>쇼콜)초코드라이브딸기<br>1,200원</b>
 				</div>
 			</div>
 		</div>
@@ -49,107 +45,77 @@
 			<div class="snack_box">
 				<img src="image/snack_5.jpg" alt="">
 				<br>
-				<b>농심)포스틱
-					<br>1,400원</b>
-				</img>
+				<b>농심)포스틱<br>1,400원</b>
 			</div>
 			<div class="snack_box">
 				<img src="image/snack_6.jpg" alt="">
 				<br>
-				<b>명도)도라에몽치즈모찌
-					<br>1,500원</b>
-				</img>
+				<b>명도)도라에몽치즈모찌<br>1,500원</b>
 			</div>
 			<div class="snack_box">
 				<img src="image/snack_7.jpg" alt="">
 				<br>
-				<b>오리온)통크
-					<br>1,200원</b>
-				</img>
+				<b>오리온)통크<br>1,200원</b>
 			</div>
 			<div class="snack_box">
 				<img src="image/snack_8.jpg" alt="">
 				<br>
-				<b>오리온)포카칩어니언
-					<br>1,500원</b>
-				</img>
+				<b>오리온)포카칩어니언<br>1,500원</b>
 			</div>
 			<div class="snack_box">
 				<img src="image/snack_9.jpg" alt="">
 				<br>
-				<b>몬스터초코라이스칩
-					<br>1,500원</b>
-				</img>
+				<b>몬스터초코라이스칩<br>1,500원</b>
 			</div>
 			<div class="snack_box">
 				<img src="image/snack_10.jpg" alt="">
 				<br>
-				<b>롯데)화이트쿠키빼빼로
-					<br>1,500원</b>
-				</img>
+				<b>롯데)화이트쿠키빼빼로<br>1,500원</b>
 			</div>
 			<div class="snack_box">
 				<img src="image/snack_11.jpg" alt="">
 				<br>
-				<b>롯데)마가렛트고로케맛
-					<br>4,400원</b>
-				</img>
+				<b>롯데)마가렛트고로케맛<br>4,400원</b>
 			</div>
 			<div class="snack_box">
 				<img src="image/snack_12.jpg" alt="">
 				<br>
-				<b>HEYROO쌀떡볶이
-					<br>1,200원</b>
-				</img>
+				<b>HEYROO쌀떡볶이<br>1,200원</b>
 			</div>
 			<div class="snack_box">
 				<img src="image/snack_13.jpg" alt="">
 				<br>
-				<b>농심)깐풍새우깡
-					<br>1,200원</b>
-				</img>
+				<b>농심)깐풍새우깡<br>1,200원</b>
 			</div>
 			<div class="snack_box">
 				<img src="image/snack_14.jpg" alt="">
 				<br>
-				<b>프링글스)이탈리안피자110
-					<br>3,300원</b>
-				</img>
+				<b>프링글스)이탈리안피자110<br>3,300원</b>
 			</div>
 			<div class="snack_box">
 				<img src="image/snack_15.jpg" alt="">
 				<br>
-				<b>롯데)치토스콘스프맛
-					<br>1,500원</b>
-				</img>
+				<b>롯데)치토스콘스프맛<br>1,500원</b>
 			</div>
 			<div class="snack_box">
 				<img src="image/snack_16.jpg" alt="">
 				<br>
-				<b>롯데)쌀로별오리지널
-					<br>1,500원</b>
-				</img>
+				<b>롯데)쌀로별오리지널<br>1,500원</b>
 			</div>
 			<div class="snack_box">
 				<img src="image/snack_17.jpg" alt="">
 				<br>
-				<b>오리온)태양의맛썬
-					<br>1,500원</b>
-				</img>
+				<b>오리온)태양의맛썬<br>1,500원</b>
 			</div>
 			<div class="snack_box">
 				<img src="image/snack_18.jpg" alt="">
 				<br>
-				<b>롯데)씨리얼오트컵
-					<br>2,500원</b>
-				</img>
+				<b>롯데)씨리얼오트컵<br>2,500원</b>
 			</div>
 			<div class="snack_box">
 				<img src="image/snack_19.jpg" alt="">
 				<br>
-				<b>롯데)몽쉘치즈케이크6입
-					<br>3,000원</b>
-				</img>
+				<b>롯데)몽쉘치즈케이크6입<br>3,000원</b>
 			</div>
 		</div>
 		</div>
