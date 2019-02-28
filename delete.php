bc39d899fc705e7058c594d944ad4fd46149af22
park-jihee <brs0705@naver.com>
2019-02-28 15:03:36 +0900

    sql delete

--- delete.php
+++ delete.php
@@ -2,13 +2,15 @@
 include("config.php");
 $count = 0;
 $id = 0;
+
 if( isset($_POST['id']) ){
- $id = (int)$_POST['id'];
+    $id = (int)$_POST['id'];
 }
 if( $id && $login ) {
- $sql = "DELETE FROM cunews WHERE id={$id}";
- $rs = $db->query($sql);
- $count = $rs->rowCount();
-}
+    $sql = "DELETE FROM cunews WHERE id={$id}";
+    $rs = $db->query($sql);
+    $count = $rs->rowCount();
+}   
+
 echo $count;
 
